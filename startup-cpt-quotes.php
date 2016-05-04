86cddbf7715cc6acdc994dd29fdaf51ed21c308f
yozz <yozz.online@gmail.com>
2016-05-04 10:37:43 -0400

    Dependencies

--- startup-cpt-quotes.php
+++ startup-cpt-quotes.php
@@ -16,6 +16,9 @@ if ( ! defined( 'WPINC' ) ) {
 //Include this to check if a plugin is activated with is_plugin_active
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 
+//Include this to check dependencies
+include_once( 'inc/dependencies.php' );
+
 //GitHub Plugin Updater
 function startup_cpt_quotes_updater() {
 	include_once 'lib/updater.php';
@@ -112,25 +115,6 @@ function startup_cpt_quotes_caps() {
 register_activation_hook( __FILE__, 'startup_cpt_quotes_caps' );
 
 // Metaboxes
-/**
- * Detection de CMB2. Identique dans tous les plugins.
- */
-if ( !function_exists( 'cmb2_detection' ) ) {
-    function cmb2_detection() {
-        if ( !defined( 'CMB2_LOADED' ) ) {
-            add_action( 'admin_notices', 'cmb2_notice' );
-        }
-    }
-
-    function cmb2_notice() {
-        if ( current_user_can( 'activate_plugins' ) ) {
-            echo '<div class="error message"><p>' . __( 'CMB2 plugin or StartUp Reloaded theme must be active to use custom metaboxes.', 'startup-cpt-quotes' ) . '</p></div>';
-        }
-    }
-
-    add_action( 'init', 'cmb2_detection' );
-}
-
 function startup_cpt_quotes_meta() {
     
 	// Start with an underscore to hide fields from custom fields list
